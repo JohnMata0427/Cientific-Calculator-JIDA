0e3f3c5ab1f58979168c64a81249671b7a57e337
JohnMata0427 <john.mata@epn.edu.ec>
2024-08-25 21:48:05 -0500

    Design improvement and adding Jython jar

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
+import java.awt.geom.RoundRectangle2D;
+
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculadora");
         frame.setContentPane(new form1().calculator);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setSize(500, 500);
+        frame.setSize(475, 575);
         frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setShape(new RoundRectangle2D.Double(0, 0, 475, 575, 15, 15));
     }
 }
